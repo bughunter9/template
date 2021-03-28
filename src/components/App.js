d4641e9d692e3c36fcc844c45f9930b8c5fd2a4d
MANROOP PARMAR <manroopsingh120@gmail.com>
2021-03-28 20:29:37 +0530

    Template

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h3 style={{textAlign:"center", fontSize:"40px"}}>Feel free to modify the code as per your needs :()</h3>
+      </header>
+    </div>
+  );
+}
+
+export default App;
